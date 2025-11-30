68b8df3de0a57032d17aa9add623b235aaa5080a
amethystCyber <marceloxc2020@gmail.com>
2025-11-30 15:36:12 +0000

    yuh

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const path = require("path");
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+
 const app = express();
+
+app.use(cors()); // allows frontend requests
+app.use(bodyParser.json()); // allows JSON request bodies
+app.use(bodyParser.urlencoded({ extended: true })); // allows form submissions
 
 // Serve files from the public folder
 app.use(express.static(path.join(__dirname, "public")));
